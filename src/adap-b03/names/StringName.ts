573a79fd2b3439c003d16fafb566c2772de47d3c
anton <anton.frisch@fau.de>
2025-11-24 15:41:22 +0100

    b03 refactor StringName.ts

--- src/adap-b03/names/StringName.ts
+++ src/adap-b03/names/StringName.ts
@@ -5,55 +5,77 @@ import { AbstractName } from "./AbstractName";
 export class StringName extends AbstractName {
 
     protected name: string = "";
-    protected noComponents: number = 0;
 
     constructor(components: string[], delimiter: string = DEFAULT_DELIMITER) {
         super(delimiter);
-        this.name = components.join(this.delimiter);
-        this.noComponents = components.length;
+        this.name = this.buildNameFromComponents(components);
+    }
+
+    private buildNameFromComponents(components: string[]): string {
+        const parts = components.map(c => this.escapeComponent(c, DEFAULT_DELIMITER));
+        return parts.join(DEFAULT_DELIMITER);
+    }
+
+    private parseComponentsFromName(): string[] {
+        if (this.name === "") {
+            return [];
+        }
+
+        const result: string[] = [];
+        let current = "";
+        let escaped = false;
+
+        for (const ch of this.name) {
+            if (escaped) {
+                current += ch;
+                escaped = false;
+            } else if (ch === ESCAPE_CHARACTER) {
+                escaped = true;
+            } else if (ch === DEFAULT_DELIMITER) {
+                result.push(current);
+                current = "";
+            } else {
+                current += ch;
+            }
+        }
+        result.push(current);
+        return result;
     }
 
     getNoComponents(): number {
-        return this.noComponents;
+        return this.parseComponentsFromName().length;
     }
 
     getComponent(i: number): string {
-        return this.name.split(this.delimiter)[i];
+        return this.parseComponentsFromName()[i];
     }
 
     setComponent(i: number, c: string): void {
-        const components = this.name === "" ? [] : this.name.split(this.delimiter);
+        const components = this.parseComponentsFromName();
         components[i] = c;
-        this.name = components.join(this.delimiter);
-        this.noComponents = components.length;
+        this.name = this.buildNameFromComponents(components);
     }
 
     insert(i: number, c: string): void {
-        const components = this.name === "" ? [] : this.name.split(this.delimiter);
+        const components = this.parseComponentsFromName();
         components.splice(i, 0, c.toString());
-        this.name = components.join(this.delimiter);
-        this.noComponents = components.length;
+        this.name = this.buildNameFromComponents(components);
     }
 
     append(c: string): void {
-        if (this.name === "") {
-            this.name = c.toString();
-            this.noComponents = 1;
-        } else {
-            this.name = this.name + this.delimiter + c.toString();
-            this.noComponents += 1;
-        }
+        const components = this.parseComponentsFromName();
+        components.push(c.toString());
+        this.name = this.buildNameFromComponents(components);
     }
 
     remove(i: number): void {
-        const components = this.name === "" ? [] : this.name.split(this.delimiter);
+        const components = this.parseComponentsFromName();
         components.splice(i, 1);
-        this.name = components.join(this.delimiter);
-        this.noComponents = components.length;
+        this.name = this.buildNameFromComponents(components);
     }
 
     clone(): Name {
-        const components = this.name === "" ? [] : this.name.split(this.delimiter);
+        const components = this.parseComponentsFromName();
         return new StringName(components, this.delimiter);
     }
 }
